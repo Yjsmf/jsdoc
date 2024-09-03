c8d31e7c158fcf8e079c72990b17afeea194d54b
kimyejin <yjin@goatfarm.ai>
2024-09-03 15:21:31 +0900

    first commit

---
+++ jsdoc-practice/README.md
@@ -0,0 +1,7 @@
+# 사칙연산 문서
+
+## [addition](global.html#addition)
+
+## [subtraction](global.html#subtraction)
+
+## [multiplication](global.html#multiplication)
---
+++ jsdoc-practice/src/index.js
@@ -0,0 +1,9 @@
+/**
+*두 개의 숫자를 바아서 합한 값을 리턴하는 add 함수
+* @param {number} number1 - 첫 번째 숫자
+* @param {number} number2 - 두 번쨰 숫자
+* @returns {number} number1 + number2
+*/
+function add(number1,number2){
+    return number1 + number2;
+}
